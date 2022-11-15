850e907918e79537256796bada51961fb0d38cb9
Ian Shi <72175372+lifeablestuff@users.noreply.github.com>
2022-11-15 14:51:09 -0800

    Update vr1.py

--- vr1.py
+++ vr1.py
@@ -36,7 +36,7 @@ class game(Fl_Window):
 		self.resizable(self)
 	
 	
-	def kill_last_process(self,pid):
+	def kill_process(self,pid):
 		'''
 		for line in os.popen("ps ax | grep " + name + " | grep -v grep"):
 			fields = line.split()
@@ -62,10 +62,36 @@ class game(Fl_Window):
     
 
                     # make setters and getters
-    def set_color(self,widget,color):
-        widget.color(color)
+	def set_color(self,widget_and_color):
+		
+		if len(widget_and_color) == 1:
+			if widget_and_color[0] == 'red':
+				self.red.color(FL_RED)
+			elif widget_and_color[0] == 'blue':
+				self.blue.color(FL_BLUE)
+			elif widget_and_color[0] == 'yellow':
+				self.yellow.color(FL_YELLOW)
+			else:
+				self.green.color(FL_GREEN)
+				
+		else:
+			if widget_and_color[0] == 'red':
+				self.red.color(FL_WHITE)
+			
+			elif widget_and_color[0] == 'blue':
+				self.blue.color(FL_WHITE)
+			
+			elif widget_and_color[0] == 'yellow':
+				self.yellow.color(FL_WHITE)
+	
+			else:
+				self.green.color(FL_WHITE)
 
-	def reactive(self):
+		self.redraw()
+	
+	
+
+	def activate_buttons(self):
 		self.red.activate()
 		self.blue.activate()
 		self.green.activate()
@@ -74,27 +100,14 @@ class game(Fl_Window):
 	def flash(self):
 		self.sound = str(os.path.join(self.working_dir,f'{self.sequence[self.temp]}.mp3'))
 		self.sound = subprocess.Popen(['vlc','--intf','dummy',self.sound])
-		Fl.add_timeout(5.0,self.kill_last_process,self.sound.pid)
+		Fl.add_timeout(5.0,self.kill_process,self.sound.pid)
 
-		if self.sequence[self.temp] == 'red':
-			self.set_color(self.red,FL_WHITE)
-		elif self.sequence[self.temp] == 'blue':
-			self.set_color(self.blue,FL_WHITE)
-		elif self.sequence[self.temp] == 'yellow':
-			self.set_color(self.yellow,FL_WHITE)
-		elif self.sequence[self.temp] == 'green':
-			self.set_color(self.green,FL_WHITE)
+		self.set_color([self.sequence[self.temp],'white'])
+		Fl.add_timeout(0.5,self.set_color,[self.sequence[self.temp]])
+		
 		self.redraw()
-		Fl.add_timeout(0.4,self.change_back)
 		self.temp += 1
-
-	def change_back(self):
-		self.red.color(FL_RED)
-		self.blue.color(FL_BLUE)
-		self.yellow.color(FL_YELLOW)
-		self.green.color(FL_GREEN)
-		self.redraw()
-
+		
 	def next_sequence(self):
 		self.red.deactivate()
 		self.blue.deactivate()
@@ -105,8 +118,8 @@ class game(Fl_Window):
 
 		self.sequence.append(random.choice(['yellow','blue','red','green']))
 		for x in range(len(self.sequence)):
-			Fl.add_timeout(1.0+ 1.0*x,self.flash)
-			Fl.add_timeout(1.0+1.0*len(self.sequence),self.reactive)
+			Fl.add_timeout(1.25+ 1.025*x,self.flash)
+			Fl.add_timeout(1.0+1.0*len(self.sequence),self.activate_buttons)
 
 		Fl.add_timeout(1.0+1.0*len(self.sequence)+5.0,self.notime)
 
@@ -128,10 +141,10 @@ class game(Fl_Window):
 					correct = True
 
 			else:
-				Fl.add_timeout(3.0,self.kill_last_process,self.sound.pid)
+				Fl.add_timeout(3.0,self.kill_process,self.sound.pid)
 				self.sound = str(os.path.join(self.working_dir,'error.mp3'))
 				self.sound = subprocess.Popen(['vlc','--intf', 'dummy',self.sound])
-				Fl.add_timeout(5.0,self.kill_last_process,self.sound.pid)
+				Fl.add_timeout(5.0,self.kill_process,self.sound.pid)
 				Fl.remove_timeout(self.notime)
 				fl_message(f'your score is: {len(self.sequence)}')
 				correct = False
@@ -140,7 +153,7 @@ class game(Fl_Window):
 				return None
 				break
 
-		Fl.add_timeout(1.0,self.kill_last_process,self.sound.pid)
+		Fl.add_timeout(1.0,self.kill_process,self.sound.pid)
 		Fl.add_timeout(5.0,self.notime)
 
 		if correct == True:
@@ -149,7 +162,7 @@ class game(Fl_Window):
 
 		print('self.pressed: ' + str(self.pressed))
 
-    def notime(self):
+	def notime(self):
 		Fl.remove_timeout(self.notime)
 		fl_message(f'your score is: {len(self.sequence)}')
 		self.sound = str(os.path.join(self.working_dir,'error.mp3'))
@@ -163,10 +176,8 @@ class game(Fl_Window):
 		if self.sequence != []:
 			fl_message(f'your score is: {len(self.sequence)}')
 		Fl.remove_timeout(self.flash)
-		Fl.remove_timeout(self.change_back)
-		Fl.remove_timeout(self.reactive)
-		self.reactive()
-		self.change_back()
+		Fl.remove_timeout(self.activate_buttons)
+		self.activate_buttons()
 		Fl.add_timeout(1.0,self.next_sequence)
 		self.pressed = []
 		self.sequence = []
